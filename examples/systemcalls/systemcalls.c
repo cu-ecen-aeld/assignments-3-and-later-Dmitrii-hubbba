bf7c88822946cc568fcece88a2e3b78c19103274
Dmitrii <dmitriysch92@gmail.com>
2023-06-09 11:04:14 +0300

    Modified systemcalls.c and .h

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,11 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+   if (system(cmd) == -1) {
+     return false;
+   } else {
+     return true;
+   }
 }
 
 /**
@@ -58,6 +61,14 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int wstatus = 0;
+    if ( fork() == -1) {
+      return false;
+    }
+    if (execv(command[0], &command[1]) == -1) {
+      return false;
+    }
+    wait(&wstatus);
 
     va_end(args);
 
@@ -92,6 +103,16 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int fd = open("redirected.txt", O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    int wstatus = 0;
+    if (fork() == -1) {
+      return false;
+    }
+    if (execv(command[0], &command[1]) == -1) {
+      return false;
+    }
+    wait(&wstatus);
+    close(fd);
 
     va_end(args);
 
