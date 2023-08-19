4e5f9e687bca23e6237cd795e57b27f024609757
Dmitrii <dmitriysch92@gmail.com>
2023-08-19 15:59:18 +0300

    Fixed conf files locations for qemu.

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,35 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
@@ -41,7 +9,7 @@ NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 #WRITEDIR=/tmp/assignment4-result.txt
-username=$(cat conf/username.txt)
+username=$(cat /etc/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -65,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
